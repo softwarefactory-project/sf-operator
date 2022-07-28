6f746309b42265ee036605d8630c4e0a28abc406
Fabien Boucher <fboucher@redhat.com>
2022-07-28 15:51:27 +0000

    Change gerrit site path to /gerrit
    This avoid for the need to mount multiple volumes to
    preserve /var/gerrit that is pre-populated at container
    image creation.
    
    This change simplifies gerrit setup by keeping only one
    volume "gerrit-data" for gerrit site data.
    
    Change-Id: I12659334d9256c704ca3183b1aa7ec0200629580

--- controllers/static/gerrit/entrypoint.sh
+++ controllers/static/gerrit/entrypoint.sh
@@ -2,12 +2,14 @@
 
 set -ex
 
+GERRIT_SITE="/gerrit"
+
 # The /dev/./urandom is not a typo. https://stackoverflow.com/questions/58991966/what-java-security-egd-option-is-for
 JAVA_OPTIONS="-Djava.security.egd=file:/dev/./urandom"
-JAVA_OPTIONS="${JAVA_OPTIONS} -Djavax.net.ssl.keyStore=/var/gerrit/etc/keystore"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Djavax.net.ssl.keyStore=${GERRIT_SITE}/etc/keystore"
 JAVA_OPTIONS="${JAVA_OPTIONS} -Djavax.net.ssl.keyStorePassword=${GERRIT_KEYSTORE_PASSWORD}"
-JAVA_OPTIONS="${JAVA_OPTIONS} -Djavax.net.ssl.trustStore=/var/gerrit/etc/truststore"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Djavax.net.ssl.trustStore=${GERRIT_SITE}/etc/truststore"
 JAVA_OPTIONS="${JAVA_OPTIONS} -Djavax.net.ssl.trustStorePassword=changeit"
 
 echo "Running Gerrit ..."
-exec java ${JAVA_OPTIONS} -jar /var/gerrit/bin/gerrit.war daemon -d /var/gerrit
+exec java ${JAVA_OPTIONS} -jar /var/gerrit/bin/gerrit.war daemon -d ${GERRIT_SITE}
