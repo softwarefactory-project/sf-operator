18bdbdf6fb44af261f31c62ef23b581d218746e2
Fabien Boucher <fboucher@redhat.com>
2023-04-03 15:42:55 +0000

    Apply security context for mariadb deployment
    Depends-on: https://softwarefactory-project.io/r/c/containers/+/28009
    
    This will remove the log message:
    
    KubeAPIWarningLogger    would violate PodSecurity "restricted:latest": allowPrivilegeEscalation != false (container "mariadb" must set securityContext.allowPrivilegeEscalation=false), unrestricted capabilities (container "mariadb" must set securityContext.capabilities.drop=["ALL"]), runAsNonRoot !=
     true (pod or container "mariadb" must set securityContext.runAsNonRoot=true), seccompProfile (pod or container "mariadb" must set securityContext.seccompProfile.type to "RuntimeDefault" or "Localhost")
    
    Change-Id: Ia71c6a63478569326937655bcfc3e5bfd29b5db2

--- controllers/mysql.go
+++ controllers/mysql.go
@@ -9,7 +9,7 @@ import (
 	apiv1 "k8s.io/api/core/v1"
 )
 
-const DBImage = "quay.io/software-factory/mariadb:10.5.16-1"
+const DBImage = "quay.io/software-factory/mariadb:10.5.16-2"
 
 const MYSQL_PORT = 3306
 const MYSQL_PORT_NAME = "mariadb-port"
@@ -19,8 +19,9 @@ func (r *SFController) EnsureDBInit(name string) ([]apiv1.Container, apiv1.Secre
 	c := "CREATE DATABASE IF NOT EXISTS " + name + " CHARACTER SET utf8 COLLATE utf8_general_ci; "
 	g := "GRANT ALL PRIVILEGES ON " + name + ".* TO '" + name + "'@'%' IDENTIFIED BY '${USER_PASSWORD}' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 	container := apiv1.Container{
-		Name:  "mariadb-client",
-		Image: DBImage,
+		Name:            "mariadb-client",
+		Image:           DBImage,
+		SecurityContext: &defaultContainerSecurityContext,
 		Command: []string{"sh", "-c", `
 echo 'Running: mysql --host=mariadb --user=root --password="$MYSQL_ROOT_PASSWORD" -e "` + c + g + `"'
 ATTEMPT=0
@@ -46,6 +47,10 @@ func (r *SFController) DeployMariadb() bool {
 	r.GenerateSecretUUID(pass_name)
 
 	dep := create_statefulset(r.ns, "mariadb", DBImage, get_storage_classname(r.cr.Spec))
+
+	dep.Spec.Template.Spec.SecurityContext = &defaultPodSecurityContext
+	dep.Spec.Template.Spec.Containers[0].SecurityContext = &defaultContainerSecurityContext
+
 	dep.Spec.VolumeClaimTemplates = append(
 		dep.Spec.VolumeClaimTemplates,
 		create_pvc(r.ns, "mariadb-logs", get_storage_classname(r.cr.Spec)))
