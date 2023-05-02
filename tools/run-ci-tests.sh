a74a8b9137670159450d50d7f43eaa68ac94c9aa
Fabien Boucher <fboucher@redhat.com>
2023-05-02 12:50:36 +0000

    Use OLM bundle to install the operator
    This change leverages the "operator bundle run" facility [1] to
    install the sf-operator via OLM on the testing node.
    
    "operator-sdk run bundle" command expects an operator-bundle image on
    a regular registry so this change also setup a temporary registry.
    
    To summarize the operator run bundle commands performs the following
    tasks:
    
    - It creates a OLM Catalog including the operator version described by
      the bundle (CSV)
    - It install the Catalog resources on the test node
    - It create a OLM Subscription to the sf-operator
    - It ensures that the OLM InstallPlan is validated and that the CRD is available
    
    Then a SofwareFactory CR can be reclaimed as usual.
    
    [1]: https://sdk.operatorframework.io/docs/olm-integration/tutorial-bundle/#deploying-an-operator-with-olm
    
    Change-Id: I5756b803a7e63d7706b99c82455fa07f36fea64e

--- tools/run-ci-tests.sh
+++ tools/run-ci-tests.sh
@@ -4,5 +4,4 @@ ansible-playbook playbooks/main.yaml \
     -e "hostname=localhost" \
     -e 'install_requirements=false' \
     -e '{"zuul":{"project":{"src_dir": ".."}}}' \
-    -e 'build_image_locally=false' \
     $*
