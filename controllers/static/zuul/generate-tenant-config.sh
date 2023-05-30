a2c912138f747fef435375c17fa345b47e238c88
Fabien Boucher <fboucher@redhat.com>
2023-05-30 08:03:09 +0000

    generate-tenant-config.sh - Add conditions when to generate at zuul startup
    This change adds:
    
    - At zuul startup (for init-scheduler-config init container) we add conditions
      to only run the tenant file generation when the main.yaml file does
      not exist yet.
    - When the script is running via the config-update flow
      (scheduler-sidecar container) we always perform the generation.
    
    Goal is to speedup the zuul-scheduler startup by removing an unneed run
    to managesf-configuration zuul as after the first startup the file is
    managed via the config-update workflow.
    
    Change-Id: I4bfd713a56d42056ee5edc8f27d73bd55cf0124b

--- controllers/static/zuul/generate-tenant-config.sh
+++ controllers/static/zuul/generate-tenant-config.sh
@@ -1,29 +1,38 @@
 #!/bin/sh
 
 set -ex
-REF=$1
-REF=${REF:-origin/master}
 
-if [ "${HOME}" == "/" ]; then
-    echo "HOME can not be / dir!"
+if [ "${HOME}" != "/var/lib/zuul" ]; then
+    echo "HOME must be /var/lib/zuul !"
     exit 1
 fi
 
-export GIT_SSH_COMMAND="ssh -i /var/lib/admin-ssh/..data/priv -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+if [ "${ZUUL_STARTUP}" == "true" -a ! -f /var/lib/zuul/main.yaml ] || [ "${ZUUL_STARTUP}" == "false" ]; then
 
-if [ -d "${HOME}/config/.git" ]; then
-  pushd "${HOME}/config"
-  git fetch origin
-  git reset --hard $REF
-  popd
+  REF=$1
+  REF=${REF:-origin/master}
+
+  export GIT_SSH_COMMAND="ssh -i /var/lib/admin-ssh/..data/priv -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+
+  # Clone or fetch config repository
+  if [ -d "${HOME}/config/.git" ]; then
+    pushd "${HOME}/config"
+    git fetch origin
+    git reset --hard $REF
+    popd
+  else
+    pushd "${HOME}"
+    git clone ssh://${CONFIG_REPO_USER}@${CONFIG_REPO_URL}
+    popd
+  fi
+
+  # Generate Zuul tenant config
+  # The HOME is a shared volume with the zuul-scheduler pod
+  managesf-configuration --gateway-url "https://${FQDN}" \
+    --cache-dir $HOME \
+    --config-dir $HOME/config \
+    --default-tenant-name internal \
+    --output $HOME/main.yaml zuul
 else
-  pushd "${HOME}"
-  git clone ssh://${CONFIG_REPO_USER}@${CONFIG_REPO_URL}
-  popd
+  echo "Conditions to run the tenant config generation did not match."
 fi
-
-managesf-configuration --gateway-url "https://${FQDN}" \
-  --cache-dir $HOME \
-  --config-dir $HOME/config \
-  --default-tenant-name internal \
-  --output $HOME/main.yaml zuul
